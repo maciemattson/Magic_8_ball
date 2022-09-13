1d7c02731cbbe1a87eefbe72e90f74936e30d4ac
maciemattson <112982388+maciemattson@users.noreply.github.com>
2022-09-13 13:15:59 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,48 @@
-let RandomNumber = 0
-input.onGesture(Gesture.Shake, function () {
-    basic.showString("WELCOME TO 8 BALL")
-    basic.showString("ASK ME ANYTHING!")
-    basic.showString("SHAKE ME TO GET AN ANSWER")
-    RandomNumber = randint(0, 3)
+input.onButtonPressed(Button.A, function () {
+    RandomNumber = randint(0, 1)
     if (RandomNumber == 0) {
-        basic.showString("UNCLEAR")
+        basic.showString("TRUTH")
     } else if (RandomNumber == 1) {
-        basic.showString("YES")
-    } else if (RandomNumber == 2) {
-        basic.showString("NO")
-    } else if (RandomNumber == 3) {
-        basic.showString("MAYBE SO")
+        basic.showString("DARE")
     }
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    ARROW_VARIABLE = randint(0, 3)
+    if (ARROW_VARIABLE == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (ARROW_VARIABLE == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (ARROW_VARIABLE == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (ARROW_VARIABLE == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 })
+let ARROW_VARIABLE = 0
+let RandomNumber = 0
+basic.showString("TRUTH OR DARE")
